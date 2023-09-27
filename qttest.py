1ffa9ccd4cccebc476bd3e9915f3c55a9b0a7cd7
comp32023 <comp32023@mail.ru>
2023-09-27 19:04:20 +0300

    save2

--- qttest.py
+++ qttest.py
@@ -17,6 +17,19 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(button)
         self.setWindowTitle("test")
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        button = QPushButton("Данилыч  и Саныч")
+
+        self.setFixedSize(QSize(500, 500))
+
+        self.setCentralWidget(button)
+        self.setWindowTitle("test")
+
 
 app = QApplication(sys.argv)
 window = MainWindow()
